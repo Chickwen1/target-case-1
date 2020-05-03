e87bf7e710ae270a4e1ffc12140d1b8be5ce7d0a
Sara Martin <miss.iserah@gmail.com>
2020-05-03 12:29:55 -0500

    added product service tests.

--- src/test/java/com/target/services/ProductServiceTests.java
+++ src/test/java/com/target/services/ProductServiceTests.java
@@ -1,12 +1,26 @@
 package com.target.services;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.target.converters.ProductConverter;
+import com.target.dtos.ProductDTO;
+import com.target.exceptions.InvalidProductException;
+import com.target.models.Product;
 import com.target.repos.ProductRepo;
 
 @RunWith(SpringRunner.class)
@@ -15,7 +29,7 @@ public class ProductServiceTests {
 	ProductService productService;
 
 	@Mock
-	ProductRepo productrepositoryMock;
+	ProductRepo productrepoMock;
 
 	/**
 	 * Setup for Mockito before any test run.
@@ -24,5 +38,24 @@ public class ProductServiceTests {
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
 	}
-
+	
+	@Test
+	public void getProductByIdTest() throws Exception {
+		Product p = new Product("13860428", "", 11.00);
+		when(productrepoMock.findCustomByProductId(Mockito.anyString())).thenReturn(p);
+		
+		productrepoMock.findCustomByProductId(p.getProductId());
+		Product actualProduct = productService.findProduct("13860428");
+		Product result = new Product("13860428", "The Big Lebowski (Blu-ray)", 11.00);
+		
+		assertEquals(result.getProductId(), actualProduct.getProductId());
+	}
+	
+	@Test(expected=InvalidProductException.class)
+	public void getProductByWrongIdTest() throws Exception {
+		Product p = new Product();
+		when(productrepoMock.findCustomByProductId(Mockito.anyString())).thenThrow(InvalidProductException.class);
+		
+		productService.findProduct(p.getProductId());
+	}
 }
